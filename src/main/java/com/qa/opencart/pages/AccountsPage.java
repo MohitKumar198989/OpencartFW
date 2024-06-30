d9219dbafbee185c57679faf0a80577b792d8b17
MohitKumar198989 <mohit.kumar198989@gmail.com>
2024-06-30 11:47:07 +0530

    @step is updated for the method get account page URl

--- src/main/java/com/qa/opencart/pages/AccountsPage.java
+++ src/main/java/com/qa/opencart/pages/AccountsPage.java
@@ -39,6 +39,7 @@ public class AccountsPage {
 		return title;
 	}
 	
+	@Step("get account page URL")
 	public String getAccPageURL()
 	{
 		String url = eleUtil.waitForURLContains(AppConstants.ACC_PAGE_URL_FRACTION, AppConstants.SHORT_DEFAUTT_WAIT);
