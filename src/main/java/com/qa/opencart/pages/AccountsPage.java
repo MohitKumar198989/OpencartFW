49e2d13983e0b5ade6c8099facdccb3c268bf1f9
MohitKumar198989 <mohit.kumar198989@gmail.com>
2024-07-06 09:52:17 +0530

    Updated the Account Page

--- src/main/java/com/qa/opencart/pages/AccountsPage.java
+++ src/main/java/com/qa/opencart/pages/AccountsPage.java
@@ -25,6 +25,10 @@ public class AccountsPage {
 	
 	
 	// page const...
+	/**
+	 * 
+	 * @param driver
+	 */
 	public AccountsPage(WebDriver driver) {
 		this.driver=driver;
 		eleUtil = new ElementUtil(this.driver);
