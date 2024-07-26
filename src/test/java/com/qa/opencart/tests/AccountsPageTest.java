c0fdbd875c2d8e477606b4ae819e613ce37a2532
MohitKumar198989 <mohit.kumar198989@gmail.com>
2024-07-26 07:48:13 +0530

    Updated AccountPageTest Page

--- src/test/java/com/qa/opencart/tests/AccountsPageTest.java
+++ src/test/java/com/qa/opencart/tests/AccountsPageTest.java
@@ -31,6 +31,7 @@ public class AccountsPageTest extends BaseTest {
 		Assert.assertTrue(accPage.getAccPageURL().contains(AppConstants.ACC_PAGE_URL_FRACTION));
 	}
 	
+	@Step("Verify the Logout link exist")
 	@Test
 	public void isLogoutLinkExistTest() {
 		Assert.assertTrue(accPage.isLogutLinkExist());
