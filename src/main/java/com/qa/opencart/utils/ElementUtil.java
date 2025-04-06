947725951680ba5045debb481c8885d66e0ada95
MohitKumar198989 <mohit.kumar198989@gmail.com>
2025-04-06 17:42:10 +0530

    update ElementUtil.java

--- src/main/java/com/qa/opencart/utils/ElementUtil.java
+++ src/main/java/com/qa/opencart/utils/ElementUtil.java
@@ -532,7 +532,7 @@ public class ElementUtil {
 	
 	@Step("waiting for the page title : {0} and titmeout: {1}")
 	/**
-	 * 
+	 * this method is used to get the title of the page
 	 * @param title
 	 * @param timeOut
 	 * @return
